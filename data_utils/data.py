05a025467b5a8cf8f1d13743fddf3f86b8161408
simon benigeri <simon.benigeri@protonmail.ch>
2022-06-07 17:14:43 -0500

    added a dataset mapping for whitespace tokenization

--- data_utils/data.py
+++ data_utils/data.py
@@ -3,7 +3,47 @@ from sklearn.model_selection import train_test_split
 
 # Note that we are using this for now because it uses average summary length
 # These numbers were on the slides and that's what David used.
+datasets_mapping_whitespace = {
+    # uses mean number of tokens from whitespace tokenization for all documents. 
+    # "big_patent": ("description", "abstract"),
+    "arxiv": ("ccdv/arxiv-summarization", "article", "abstract", "", 279),
+
+    "billsum": ("billsum", "text", "summary", "", 179),
+
+    "cnn": ("cnn_dailymail", "article", "highlights", "3.0.0", 51),
+    "cnn_dailymail": ("cnn_dailymail", "article", "highlights", "3.0.0", 51),
+
+    "gov": ("ccdv/govreport-summarization", "report", "summary", "", 477),
+    "govreport": ("ccdv/govreport-summarization", "report", "summary", "", 477),
+
+    "pubmed": ("ccdv/pubmed-summarization", "article", "abstract", "", 202),
+
+    "reddit_tifu": ("reddit_tifu", "documents", "tldr", "long", 20),
+    "tifu": ("reddit_tifu", "documents", "tldr", "long", 20),
+}
+
+datasets_mapping_nltk = {
+    # uses mean number of tokens from nltk word tokenize for all documents. 
+    # "big_patent": ("description", "abstract"),
+    "arxiv": ("ccdv/arxiv-summarization", "article", "abstract", "", 293),
+
+    "billsum": ("billsum", "text", "summary", "", 204),
+
+    "cnn": ("cnn_dailymail", "article", "highlights", "3.0.0", 56),
+    "cnn_dailymail": ("cnn_dailymail", "article", "highlights", "3.0.0", 56),
+
+    "gov": ("ccdv/govreport-summarization", "report", "summary", "", 546),
+    "govreport": ("ccdv/govreport-summarization", "report", "summary", "", 546),
+
+    "pubmed": ("ccdv/pubmed-summarization", "article", "abstract", "", 204),
+
+    "reddit_tifu": ("reddit_tifu", "documents", "tldr", "long", 23),
+    "tifu": ("reddit_tifu", "documents", "tldr", "long", 23),
+}
+
 datasets_mapping = {
+    # uses mean number of tokens from nltk word tokenize of a sample of 1000 documents. 
+    # this sample uses a length cutoff for some datasets
     # "big_patent": ("description", "abstract"),
     "arxiv": ("ccdv/arxiv-summarization", "article", "abstract", "", 200),
 
@@ -21,19 +61,6 @@ datasets_mapping = {
     "tifu": ("reddit_tifu", "documents", "tldr", "long", 23),
 }
 
-"""
-# We are setting this aside for now because it uses median summary length
-datasets_mapping = {
-    # "big_patent": ("description", "abstract"),
-    "cnn_dailymail": ("cnn_dailymail", "article", "highlights", "3.0.0", 51),
-    "arxiv": ("ccdv/arxiv-summarization", "article", "abstract", "", 169),
-    "pubmed": ("ccdv/pubmed-summarization", "article", "abstract", "", 210),
-    "reddit_tifu": ("reddit_tifu", "documents", "tldr", "long", 21),
-    "billsum": ("billsum", "text", "summary", "", 179),
-    "govreport": ("ccdv/govreport-summarization", "report", "summary", "", 563),
-}
-"""
-
 def load_dataset_huggingface(dataset: str):
     (
         dataset_name,
@@ -41,7 +68,7 @@ def load_dataset_huggingface(dataset: str):
         summary_key,
         config,
         length,
-    ) = datasets_mapping[dataset]
+    ) = datasets_mapping_whitespace[dataset] # datasets_mapping[dataset]
 
     # TODO: Don't worry about this now, but we want to download a specific split.
     # I had some weird stuff happen so just keep this like so for now. NOT A PRIORITY
